877e2924fd4bb7de669fe6e0d6b0adc24e84f679
Мамедов Тимур Арифович <Mamedov.Timur@oak.cc>
2018-01-17 13:36:14 +0300

    for travis version

--- test_allocator.cpp
+++ test_allocator.cpp
@@ -1,10 +1,14 @@
-#define BOOST_TEST_MODULE ip_filter_test
+#define BOOST_TEST_MODULE allocator_test
 
 #include <boost/test/unit_test.hpp>
 
 #include "version.h"
 
+BOOST_AUTO_TEST_SUITE(TestSuite1)
+
 BOOST_AUTO_TEST_CASE(version_test)
 {
     BOOST_CHECK( version() > 0 );
 }
+
+BOOST_AUTO_TEST_SUITE_END()
